8aba47263a50803980326efcfcf6c61b9b311924
YuriiTrofimenko <tyaa@ukr.net>
2020-11-29 21:04:06 +0200

    html->pug, css->stylus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
-<template>
-  <div>
-    Username: {{user.username}}
-  </div>
+<template lang='pug'>
+div
+| Username: {{user.username}}
 </template>
 
 <script>
@@ -23,13 +22,12 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='stylus'>
+#app 
+	font-family Avenir, Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	text-align center
+	color #2c3e50
+	margin-top 60px
 </style>
