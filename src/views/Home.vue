3e90a31de1bd53313ca368f51ee40679a3b59c01
YuriiTrofimenko <tyaa@ukr.net>
2020-12-08 17:04:49 +0200

    Home Component: User List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template lang='pug'>
-h1 Home Page
+.home
+  h1 Home
+  .users-list
+    router-link(v-for="user in users" :to="{ name: 'UserProfile', params: { userId: user.id } }" :key="user.id")
+      |{{ user.username }}
 </template>
 
 <script>
+import {users} from "../assets/users"
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    return {
+      users
+    }
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+.home
+  padding 50px 5%
+  .users-list
+    display flex
+    flex-direction column
+</style>
