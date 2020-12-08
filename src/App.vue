6bf27a82d206936e13e29336b294f220bd36c07d
YuriiTrofimenko <tyaa@ukr.net>
2020-12-08 15:41:03 +0200

    vue-router init

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,17 @@
-<template lang='pug'>
-.app
-  UserProfile
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import UserProfile from './components/UserProfile'
-export default {
-  name: 'App',
-  components: { UserProfile },
-  data () {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'YuriiTrofimenko',
-        firstName: 'Yurii',
-        lastName: 'Trofimenko',
-        email: 'tyaa@ukr.net',
-        isAdmin: true
-      }
-    }
-  },
-  mounted () {
-    this.followUser()
-  },
-  computed: {
-    fullName () {
-      return `${this.user.firstName} ${this.user.lastName}`
-    }
-  },
-  watch: {
-    followers(newFollowersCount, oldFollowersCount) {
-      if(newFollowersCount < oldFollowersCount) {
-        alert(`User ${this.user.username} has gained a follower`)
-      }
-    }
-  },
-  methods: {
-    followUser () {
-      this.followers++
-    }
-  }
-}
-</script>
-
-<style lang='stylus'>
-.app 
-	font-family Avenir, Helvetica, Arial, sans-serif
-	-webkit-font-smoothing antialiased
-	-moz-osx-font-smoothing grayscale
-	color #2c3e50
-	min-height 100vh
-	background-color #f3f5fa
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
 </style>
